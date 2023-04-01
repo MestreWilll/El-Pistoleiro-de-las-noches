55884be105683b55594ad15bc42cff79b2ec9919
MestreWilll <willianichigo@hotmail.com>
2023-04-01 07:00:09 -0300

    fix movimentos

--- Movimentação em Python teste.py
+++ Movimentação em Python teste.py
@@ -1,12 +1,20 @@
 import UnityEngine
 from UnityEngine import Vector3
+from UnityEngine import Input
+from UnityEngine import Time
+from UnityEngine import Rigidbody
 
-class Movimentacao(UnityEngine.MonoBehaviour):
-    velocidade = 10.0 # Velocidade do personagem
-    
+class PlayerMovement(UnityEngine.MonoBehaviour):
+    moveSpeed = 5.0
+
+    def Start(self):
+        self.rb = self.GetComponent[Rigidbody]()
+
     def Update(self):
-        horizontal = Input.GetAxis("Horizontal") # Entrada horizontal
-        vertical = Input.GetAxis("Vertical") # Entrada vertical
-        
-        movimento = Vector3(horizontal, 0, vertical) * self.velocidade * Time.deltaTime # Movimento do personagem
-        self.transform.position += movimento # Aplica movimento ao transform do objeto
+        move_horizontal = Input.GetAxis("Horizontal")
+        move_vertical = Input.GetAxis("Vertical")
+
+        self.move_direction = Vector3(move_horizontal, 0.0, move_vertical)
+
+    def FixedUpdate(self):
+        self.rb.MovePosition(self.transform.position + self.move_direction * self.moveSpeed * Time.fixedDeltaTime)
