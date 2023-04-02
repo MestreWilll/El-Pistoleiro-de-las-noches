b4fedee3690777ae2e07c0f584af961727336fa7
MestreWilll <willianichigo@hotmail.com>
2023-04-02 00:13:58 -0300

    fix movimentação e pulo

---
+++ Movimentação e pulo Funcional.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveChar2D : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    public float jumpForce;
+    public LayerMask groundLayer;
+
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        Vector2 movement = new Vector2(horizontalInput, 0).normalized;
+        rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(transform.position, 0.1f, groundLayer);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
