546280272c7b35d377c3b69523b9b678bf5c1130
MestreWilll <willianichigo@hotmail.com>
2023-04-14 02:03:55 -0300

    fix

--- Movimentação/MovimentaçãoLadosEPulosImplementado.cs
+++ Movimentação/MovimentaçãoLadosEPulosImplementado.cs
@@ -8,24 +8,34 @@ public class MoveChar2D : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
     public LayerMask groundLayer;
-    public float checkDistance = 0.1f;
+    public float checkDistance = 0.5f;
 
     private bool isGrounded;
     private bool jumpRequested;
 
+    public Animator animator;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        // Movement
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            animator.SetBool("Tacorrendo", true);
+        }
+        else
+        {
+            animator.SetBool("Tacorrendo", false);
+        }
+
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         Vector2 movement = new Vector2(horizontalInput, 0).normalized;
         rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
 
-        // Turning
         if (horizontalInput > 0)
         {
             transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
@@ -35,20 +45,24 @@ public class MoveChar2D : MonoBehaviour
             transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
 
-        // Check if grounded
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, checkDistance, groundLayer);
+        Vector3 raycastOrigin = transform.position + new Vector3(0, -0.5f, 0);
+        RaycastHit2D hit = Physics2D.Raycast(raycastOrigin, Vector2.down, checkDistance, groundLayer);
         isGrounded = hit.collider != null;
 
-        // Request jump
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             jumpRequested = true;
+        }
+
+        // Adicionando a animação "Atirando" ao pressionar a tecla 'F'
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("Atirando");
         }
     }
 
     void FixedUpdate()
     {
-        // Apply jump force
         if (jumpRequested)
         {
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
